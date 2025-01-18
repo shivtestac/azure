3a23fed93a7179be19fbf38bacf38e13326d2874
shivtestac <122268559+shivtestac@users.noreply.github.com>
2025-01-18 12:31:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
-<h1> Hi This is File from Index.php</h1>
+Deployment sucessfullllll
